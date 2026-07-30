c270c6a2a2b851938367a5bf05909eea8110131b
jassy12356 <jassy3477@gmail.com>
2026-07-30 18:07:59 +0500

    Initialize Firebase app and Firestore database

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4zXHG1QuBiozdZDiQlSGhn_g1_2_Jo58",
+  authDomain: "work-with-home-3cbc8.firebaseapp.com",
+  projectId: "work-with-home-3cbc8",
+  storageBucket: "work-with-home-3cbc8.firebasestorage.app",
+  messagingSenderId: "579230440177",
+  appId: "1:579230440177:web:36f7973a2d234d66966750",
+  measurementId: "G-GQZDWRWDGN"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
