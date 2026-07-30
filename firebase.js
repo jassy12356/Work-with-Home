c710f3d5b8bba74e029c26e61c999b8a0bbc1ce2
jassy12356 <jassy3477@gmail.com>
2026-07-30 23:07:50 +0500

    Update Firebase SDK to version 10.12.5

--- firebase.js
+++ firebase.js
@@ -1,5 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB4zXHG1QuBiozdZDiQlSGhn_g1_2_Jo58",
@@ -7,10 +14,11 @@ const firebaseConfig = {
   projectId: "work-with-home-3cbc8",
   storageBucket: "work-with-home-3cbc8.firebasestorage.app",
   messagingSenderId: "579230440177",
-  appId: "1:579230440177:web:36f7973a2d234d66966750"
+  appId: "1:579230440177:web:36f7973a2d234d66966750",
+  measurementId: "G-GQZDWRWDGN"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
